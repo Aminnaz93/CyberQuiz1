015103e62a9a7f204a2485c532a38dc7410b6b75
Amin Nazari <Aminnazari040@hotmail.com>
2026-02-25 10:12:40 +0100

    Add project files.

---
+++ CyberQuiz/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace CyberQuiz.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ CyberQuiz/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace CyberQuiz.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
