3860d39f7e3a6180e878966f4afe509d34e96fd5
henriksih <henrik.sihvonen@gmail.com>
2026-02-27 09:50:34 +0100

    Added repository and interface for Categories

--- CyberQuiz.DAL/Repositories/CategoryRepository.cs
+++ CyberQuiz.DAL/Repositories/CategoryRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using CyberQuiz.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CyberQuiz.DAL.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        //DependencyInjection av DbContext
         private readonly ApplicationDbContext _context;
 
         public CategoryRepository(ApplicationDbContext context)
@@ -13,6 +17,28 @@ namespace CyberQuiz.DAL.Repositories
             _context = context;
         }
 
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
 
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        // Lägg till i interface:
+        public async Task<IEnumerable<Category>> GetAllCategoriesWithSubCategoriesAsync()
+        {
+            return await _context.Categories
+                .Include(c => c.SubCategories)
+                .ToListAsync();
+        }
+        public async Task<Category?> GetCategoryWithSubCategoriesAsync(int id)
+        {
+            return await _context.Categories
+                .Include (c => c.SubCategories)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
---
+++ CyberQuiz.DAL/Repositories/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CyberQuiz.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.DAL.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category?> GetCategoryByIdAsync(int id);
+        // Lägg till i interface:
+        Task<IEnumerable<Category>> GetAllCategoriesWithSubCategoriesAsync();
+        Task<Category?> GetCategoryWithSubCategoriesAsync(int id);
+    }
+}
