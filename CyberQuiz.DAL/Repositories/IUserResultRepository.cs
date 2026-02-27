ca4da736805a3a8eae455bd410d21deb0c3632b1
henriksih <henrik.sihvonen@gmail.com>
2026-02-27 12:39:07 +0100

    Added UserResult Repository and Interface

---
+++ CyberQuiz.DAL/Repositories/IUserResultRepository.cs
@@ -0,0 +1,17 @@
+﻿using CyberQuiz.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.DAL.Repositories
+{
+    public interface IUserResultRepository
+    {
+        Task<IEnumerable<UserResult>> GetAllUserResultsByUserIdAsync(string userId);
+        Task<IEnumerable<UserResult>> GetAllUserResultsAsync();
+        Task<UserResult?> GetUserResultByIdAsync(int id);
+        Task<IEnumerable<UserResult>> GetUserProgressByUserAndSubcategoryAsync(string userId, int subCategoryId);
+        Task AddUserResultAsync(UserResult userResult);
+        Task<UserResult?> GetUserResultByUserAndQuestionAsync(string userId, int questionId);
+    }
+}
